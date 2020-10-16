b736afa018270a14cff9797dfbba9fe46d7d5b44
Burke Azbill <burkeazbill@users.noreply.github.com>
2020-10-16 16:43:41 -0400

    Update to support TKG 1.2.0, 3-digit suffix, IP Range

--- createClusters.sh
+++ createClusters.sh
@@ -5,8 +5,8 @@
 #               system.
 # author      : Burke Azbill
 # repo        : https://github.com/burkeazbill/TKG-Scripts
-# date        : 2020-05-11
-# version     : 1.1.3
+# date        : 2020-10-15
+# version     : 1.2
 # usage       : createClusters.sh <clusterCount>
 # notes       :
 # This script has been written from the start to copy config
@@ -21,15 +21,17 @@
 # cluster_name_prefix : the suffix will be in the form of ##
 # where ## = 01 -> 99 
 cluster_name_prefix="tkg-"
-# tkg_subnet : the network on which the Metal LB Load Balancer 
-#               range should be created. The last octet will be 
-#               filled dynamically
-tkg_subnet="172.25.1"
+# Setup VIP and LB Address array:
+# This should be a range of available IPS that are not in a DHCP range
+lb_ips=($(echo 172.25.{1..3}.{0..255} | tr ' ' '\012'))
+# The above string creates an array that starts at 172.25.1.0
+# and iterates through to 172.25.2.255
+
 # lb_address_count : How many addresses should be configured for 
 #               the Metal LB Config Map with the default setting 
 #               of 4, the first cluster will receive .1 -> .4, 
 #               second will receive .5 -> .8 and so on
-lb_address_count=4
+lb_address_count=2
 #### SCP SETTINGS ####
 # Path to private key used to SCP files to desktops:
 scp_private_key_path="$HOME/.ssh/id_rsa"
@@ -56,7 +58,8 @@ remote_folder="/etc/skel"
 #     their assigned TKG Cluster VMs.     
 ssh_pub_key_path="$HOME/.ssh/hol-keys/id_rsa.pub"
 # K8s version based on Photon image that is uploaded:
-k8sversion="v1.17.9+vmware.1"
+k8sversion="v1.17.11+vmware.1"
+# Alternate versions for TKG 1.2: v1.18.8-vmware.1, v1.19.1-vmware.2
 
 # TKG CLI OVERRIDES:
 # - example usage of overrides:
@@ -97,38 +100,74 @@ export VSPHERE_SSH_AUTHORIZED_KEY=`cat $ssh_pub_key_path`
 
 
 ###### You should not need to modify below this line ######
-(( lb_diff = lb_address_count -1 ))
-# Define function to convert time:
-secs_to_human() {
-    if [[ -z ${1} || ${1} -lt 60 ]] ;then
-        min=0 ; secs="${1}"
-    else
-        time_mins=$(echo "scale=2; ${1}/60" | bc)
-        min=$(echo ${time_mins} | cut -d'.' -f1)
-        secs="0.$(echo ${time_mins} | cut -d'.' -f2)"
-        secs=$(echo ${secs}*60|bc|awk '{print int($1+0.5)}')
-    fi
-    echo "Time Elapsed : ${min} minutes and ${secs} seconds."
+# hms function source:
+# https://www.shellscript.sh/tips/hms/
+echo "`date`: Starting the script."
+# Define hms() function
+hms()
+{
+  # Convert Seconds to Hours, Minutes, Seconds
+  # Optional second argument of "long" makes it display
+  # the longer format, otherwise short format.
+  local SECONDS H M S MM H_TAG M_TAG S_TAG
+  SECONDS=${1:-0}
+  let S=${SECONDS}%60
+  let MM=${SECONDS}/60 # Total number of minutes
+  let M=${MM}%60
+  let H=${MM}/60
+  
+  if [ "$2" == "long" ]; then
+    # Display "1 hour, 2 minutes and 3 seconds" format
+    # Using the x_TAG variables makes this easier to translate; simply appending
+    # "s" to the word is not easy to translate into other languages.
+    [ "$H" -eq "1" ] && H_TAG="hour" || H_TAG="hours"
+    [ "$M" -eq "1" ] && M_TAG="minute" || M_TAG="minutes"
+    [ "$S" -eq "1" ] && S_TAG="second" || S_TAG="seconds"
+    [ "$H" -gt "0" ] && printf "%d %s " $H "${H_TAG},"
+    [ "$SECONDS" -ge "60" ] && printf "%d %s " $M "${M_TAG} and"
+    printf "%d %s\n" $S "${S_TAG}"
+  else
+    # Display "01h02m03s" format
+    [ "$H" -gt "0" ] && printf "%02d%s" $H "h"
+    [ "$M" -gt "0" ] && printf "%02d%s" $M "m"
+    printf "%02d%s\n" $S "s"
+  fi
 }
-# Initialize Timer:
-start_time="$(date -u +%s)"
-# Initialize Cleanup Script
-cleanup_script="deleteClusters-"$start_time".sh"
-echo \# Cleanup Script > $cleanup_script
-chmod +x $cleanup_script
+
 if [ -n "$1" ]; then
+  ##### Initialize Timer #####
+  start_time=`date +%s`
+  ##### Initialize Cleanup Script #####
+  cleanup_script="deleteClusters-"$start_time".sh"
+  echo \# Cleanup Script > $cleanup_script
+  chmod +x $cleanup_script
+  ##### Start Address Calculations #####
+  let ips_needed=(lb_address_count + 1)
+  echo "IPs needed per cluster: "$ips_needed
+  total_avail_ips=${#lb_ips[@]}
+  echo "Total Available IPs: "$total_avail_ips
+  let total_required_ips=( $1 * $ips_needed )
+  echo "Total Required IPs: "$total_required_ips
+  echo "#############################################"
+  # Make sure there are enough IP Addresses available
+  # for each Cluster VIP and MetalLB Pool
+  if [[ $total_required_ips -gt $total_avail_ips ]]; then
+    echo "Insufficient IPs to meet deployment requirements"
+    exit
+  fi
+  (( lb_diff = $lb_address_count - 1 ))
+  ##### Begin Main creation loop #####
   for i in $(seq 1 "$1"); do
-    c_start="$(date -u +%s)"
-    if [ "$i" -lt 10 ]; then
-      cluster_number="0$i"
-    else
-      cluster_number="$i"
-    fi
+    c_start=`date +%s`
+    cluster_number=`printf %03d $i`
     clustername="$cluster_name_prefix$cluster_number"
+    echo $clustername
+    # Get Cluster VIP address
     # Define Load Balancer Range:
-    (( lb_end = i*lb_address_count ))
-    (( lb_start = lb_end - lb_diff ))
-    lb_range="$tkg_subnet.$lb_start-$tkg_subnet.$lb_end"
+    (( lb_end = i*$ips_needed ))
+    (( cluster_ip = $lb_end - $lb_address_count ))
+    (( lb_start = $lb_end - $lb_diff ))
+    lb_range="${lb_ips[$lb_start]}-${lb_ips[$lb_end]}"
     # Now generate the MetalLB for Layer 2 as per: https://metallb.universe.tf/configuration/
 cat  <<EOF > metal-lb-$cluster_number.yaml
 apiVersion: v1
@@ -149,8 +188,9 @@ EOF
     echo "kubeconfig and metal-lb yaml will be placed in /etc/skel"
     echo "which means it will be in each user home directory upon first login"
     echo "With LB Range: $lb_range"
+    echo "And Cluster VIP: "${lb_ips[$cluster_ip]}
     # The following line updated to control size of nodes and allow for K8s version selection
-    tkg create cluster $clustername -c 1 -w 2 -p dev --controlplane-size medium  --haproxy-size small --kubernetes-version $k8sversion
+    tkg create cluster $clustername -c 1 -w 2 -p dev --controlplane-size medium --worker-size medium  --kubernetes-version $k8sversion --vsphere-controlplane-endpoint-ip ${lb_ips[$cluster_ip]}
     echo tkg delete cluster $clustername -y >> $cleanup_script
     echo kubectl config unset clusters.$clustername >> $cleanup_script
     echo kubectl config unset contexts.$clustername-admin@$clustername >> $cleanup_script
@@ -167,16 +207,18 @@ EOF
       scp -i $scp_private_key_path -o StrictHostKeyChecking=no ./$clustername.kubeconfig $remote_ssh_username@$remote_system:$remote_folder
       scp -i $scp_private_key_path -o StrictHostKeyChecking=no ./metal-lb-$cluster_number.yaml $remote_ssh_username@$remote_system:$remote_folder
     fi
-    c_end="$(date -u +%s)"
-    c_elapsed="$(($c_end-$c_start))"
-    echo "== Cluster Create Time =="
-    secs_to_human $c_elapsed
-    echo ""
+    c_end=`date +%s`
+    let c_elapsed=${c_end}-${c_start}
+    echo "===== Cluster Create Time ====="
+    cluster_time=`hms $c_elapsed`
+    echo "Cluster creation time: ${cluster_time}"
+    echo "#############################################"
   done
-  end_time="$(date -u +%s)"
-  elapsed="$(($end_time-$start_time))"
+  end_time=`date +%s`
+  let elapsed=${end_time}-${start_time}
   echo "=================== Total Script Run Time ========================="
-  secs_to_human $elapsed
+  total_time=`hms $elapsed long`
+  echo "Total script time: ${total_time}"
 else
   echo "Usage: createClusters.sh <NumOfClusters>"
   echo "IE: createClusters.sh 5"
